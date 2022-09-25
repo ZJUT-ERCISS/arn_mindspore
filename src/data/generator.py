7efbcf27b4db4e69897d69e91f4ce1f5b4d84e07
ZJUT-ERCISS <114212416+ZJUT-ERCISS@users.noreply.github.com>
2022-09-25 17:12:22 +0800

    Update generator.py

--- src/data/generator.py
+++ src/data/generator.py
@@ -96,6 +96,7 @@ class DatasetGenerator:
             num_frame = int(pos_end * frame_len - pos_start * frame_len)
             label = self.label[item][0]
         sample_list = self.__sampler__(num_frame=num_frame, action_start=action_start)
+        sample_list = np.concatenate(sample_list)
         if self.suffix == "picture":
             video = [video[sample_list[i]] for i in range(self.num_clips)]
         if self.suffix == "video":
